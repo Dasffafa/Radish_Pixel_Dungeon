50765700dddabe93b151de1e16063da3cd2af716
Prana Ling <70191651+LingASDJ@users.noreply.github.com>
2025-05-16 19:52:32 +0800

    牧师1-2阶解封

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/HeroSelectScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/HeroSelectScene.java
@@ -55,7 +55,6 @@ import com.watabou.noosa.NinePatch;
 import com.watabou.noosa.PointerArea;
 import com.watabou.noosa.tweeners.Tweener;
 import com.watabou.noosa.ui.Component;
-import com.watabou.utils.DeviceCompat;
 import com.watabou.utils.GameMath;
 import com.watabou.utils.PointF;
 
@@ -132,10 +131,10 @@ public class HeroSelectScene extends PixelScene {
 			protected void onClick() {
 				super.onClick();
 
-				if(GamesInProgress.selectedClass == HeroClass.RECTOR && !DeviceCompat.isDebug()){
-					ShatteredPixelDungeon.scene().addToFront(new WndMessage(Messages.get(HeroClass.class, "rector_unlock")));
-					return;
-				}
+//				if(GamesInProgress.selectedClass == HeroClass.RECTOR && !DeviceCompat.isDebug()){
+//					ShatteredPixelDungeon.scene().addToFront(new WndMessage(Messages.get(HeroClass.class, "rector_unlock")));
+//					return;
+//				}
 
 				if (GamesInProgress.selectedClass == null) return;
 
