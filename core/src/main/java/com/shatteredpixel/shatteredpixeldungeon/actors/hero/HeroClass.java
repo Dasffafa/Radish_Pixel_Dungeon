b3ebf13b9808241eb3cff680f3f50c85dbda4259
DoggingDog <chh1916308908@gmail.com>
2025-05-20 16:14:48 +0800

    # 传承装备更新 - 传承·宵暗新建文件夹 # LagacyItem update - Showdarker init

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -70,6 +70,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.Waterskin;
 import com.shatteredpixel.shatteredpixeldungeon.items.alive.SeedOfCard;
 import com.shatteredpixel.shatteredpixeldungeon.items.alive.StoneOfCard;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClothArmor;
+import com.shatteredpixel.shatteredpixeldungeon.items.lagacyItem.Showdarker;
 import com.shatteredpixel.shatteredpixeldungeon.items.lagacyItem.Turtleir;
 import com.shatteredpixel.shatteredpixeldungeon.items.lagacyItem.Wastelandew;
 import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.CloakOfShadows;
@@ -143,6 +144,7 @@ public enum HeroClass {
 		new Chibayari().identify().collect();
 		new Wastelandew().identify().collect();
 		new Turtleir().identify().collect();
+		new Showdarker().identify().collect();
 
 		new SpawnMisc().collect();
 		new MobPlacer().collect();
