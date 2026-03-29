696c4341ba394f21ed5650c275bbcb21c19180e1
TheCatist <startracer@163.com>
2026-03-29 22:55:36 +0800

    修改了电脑版调试时间挑战中背包按钮不能全部显示的严重问题。

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndBag.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndBag.java
@@ -437,6 +437,12 @@ public class WndBag extends WndTabbed {
 					return SPDAction.BAG_4;
 				case 5:
 					return SPDAction.BAG_5;
+				case 6:
+					return SPDAction.BAG_6;
+				case 7:
+					return SPDAction.BAG_7;
+				case 8:
+					return SPDAction.BAG_8;
 			}
 		}
 
